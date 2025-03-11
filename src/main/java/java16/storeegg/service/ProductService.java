b24ec3415ac91bc3145a29d7e3c382dd5e5c9bfd
Aibek <clesh.meder.271@gmail.com>
2025-03-11 16:44:51 +0600

    add .

--- src/main/java/java16/storeegg/service/ProductService.java
+++ src/main/java/java16/storeegg/service/ProductService.java
@@ -5,6 +5,11 @@ import java16.storeegg.models.Product;
 import java.util.List;
 
 public interface ProductService {
+
+
+
+    void addProduct();
+
     List<Product> getAll();
 
     Product findById(Long id);
